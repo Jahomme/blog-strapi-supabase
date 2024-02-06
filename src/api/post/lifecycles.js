3238ad12467e36480b669ba7ab23126afd434a5a
Jadilson <jadilsonigor@gmail.com>
2024-02-06 18:07:54 -0300

    Atualizadas os lifecycles-hook afterCreate e afterUpdate

--- src/api/post/lifecycles.js
+++ src/api/post/lifecycles.js
@@ -1,12 +1,16 @@
 const axios = require("axios");
 
 module.exports = {
-  async afterCreate(result, data) {
+  async afterCreate(event) {
+    const { result, data } = event;
+
     axios.post(
       "https://api.vercel.com/v1/integrations/deploy/prj_Sr32kYhDI56BnBqNb0HKZMQvaoSX/9K6M5sjeet"
     );
   },
-  async afterUpdate(result, params, data) {
+  async afterUpdate(event) {
+    const { result, params, data } = event;
+
     axios.post(
       "https://api.vercel.com/v1/integrations/deploy/prj_Sr32kYhDI56BnBqNb0HKZMQvaoSX/9K6M5sjeet"
     );
